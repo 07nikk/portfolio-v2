8b38e4f865579955651f721a3b4e45f7a5f8a366
07nikk <nikhilthakur0698@gmail.com>
2025-07-10 10:26:09 +0530

    commiting loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,41 @@
-import { Routes, Route } from 'react-router-dom';
-import './App.scss';
-import Navbar from './components/Navbar/Navbar';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import CreativeCoding from './pages/Projects';
+import { Routes, Route } from "react-router-dom";
+import "./App.scss";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import CreativeCoding from "./pages/Projects";
 
-
+import { useState, useEffect } from "react";
+import Loader from "./components/Loader";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    //Loading delay
+    const timer = setTimeout(() => setLoading(false), 1500);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
     <div>
       <div className="App-container">
-      {/* <MySketch /> */}
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<CreativeCoding />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+        {/* <MySketch /> */}
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<CreativeCoding />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
       </div>
-      
     </div>
   );
 }
 
 export default App;
-
-
-
