f7dde465a881074477bc5f9e0982c93f10176768
07nikk <nikhilthakur0698@gmail.com>
2025-07-10 15:37:01 +0530

    fixing page load

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import './Navbar.scss';
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import "./Navbar.scss";
 
 const Navbar = () => {
   const location = useLocation();
-  
+
   return (
     <nav className="navbar">
       <div className="navbar__links">
         <Link to="/">Nikhil Thakur</Link>
       </div>
       <div className="navbar__links">
-        <Link to="/" className={location.pathname === '/' ? 'active' : ''}>
+        <Link to="/" className={location.pathname === "/" ? "active" : ""}>
           Home
         </Link>
-        <Link to="/about" className={location.pathname === '/about' ? 'active' : ''}>
+        <Link
+          to="/about"
+          className={location.pathname === "/about" ? "active" : ""}
+        >
           About
         </Link>
-        <Link to="/projects" className={location.pathname === '/projects' ? 'active' : ''}>
+        <Link
+          to="/projects"
+          className={location.pathname === "/projects" ? "active" : ""}
+        >
           Projects
         </Link>
-        <Link to="/contact" className={location.pathname === '/contact' ? 'active' : ''}>
+        <Link
+          to="/contact"
+          className={location.pathname === "/contact" ? "active" : ""}
+        >
           Contact
         </Link>
       </div>
